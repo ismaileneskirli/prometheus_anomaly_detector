4e50f6b28e15cef6a9d841f78a6a73fcf1d744c4
ismaileneskirli <69651222+ismaileneskirli@users.noreply.github.com>
2021-03-09 15:53:57 +0300

    Apı works + postgresql implemented.

--- prometheus/python-application/src/server.py
+++ prometheus/python-application/src/server.py
@@ -1,19 +1,80 @@
-from flask import Response, Flask, request,render_template
+from flask import Response, Flask, request,render_template, jsonify
 import prometheus_client
 from prometheus_client.core import CollectorRegistry
 from prometheus_client import Summary, Counter, Histogram, Gauge
 import time
 import random
+import os
+import googleapiclient.discovery
+import logging
+from sqlalchemy import create_engine
+
+DB_NAME = 'anomaly'
+DB_USER = 'admin'
+DB_PASSWORD = 'admin'
+DB_HOST ='db'
+DB_PORT = '5432'
+
+# connect to the database here.
+
+db_string = 'postgres://{}:{}@{}:{}/{}'.format(DB_USER,DB_PASSWORD,DB_HOST,DB_PORT,DB_NAME)
+db = create_engine(db_string)
+
+def add_new_row(n):
+    # Insert a new number into the 'numbers' table.
+    db.execute("INSERT INTO numbers (number,timestamp) "+\
+        "VALUES ("+ \
+        str(n) + "," + \
+        str(int(round(time.time() * 1000))) + ");")
+
+def get_all():
+    # Get all of the data from the db
+    rows = []
+    query = "SELECT * FROM numbers"
+    result = db.execute(query)
+    print(f"Selected {result.rowcount} rows.")
+    for row in result.fetchall():
+        rows.append(row[0])
+    return(jsonify(rows))
 
 app = Flask(__name__)
 
 _INF = float("inf")
 
 anomaly_series = [1,1,-1,1,1,1,1,1,1,-1]
-graphs = {} 
+graphs = {}
 graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
 graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.', buckets=(1, 2, 5, 6, 10, _INF))
 graphs['f'] = Gauge ('anomaly_detector_output','Anomaly model outputs, -1 means outlier value')
+
+ ########################################### Anomaly API #############################################################
+def predict_json(project, model, instances, data, version=None):
+    service = googleapiclient.discovery.build('ml', 'v1')
+    name = 'projects/{}/models/{}'.format(project, model)
+
+    if version is not None:
+        name += '/versions/{}'.format(version)
+
+    print("name: {}".format(name))
+    response = service.projects().predict(
+        name=name,
+        body={'instances': instances, 'data':data}
+    ).execute()
+
+    if 'error' in response:
+        raise RuntimeError(response['error'])
+
+    return response['predictions']
+
+# Create Dummy Data.
+def generateArray(n):
+    array = []
+    for i in range (n):
+        array.append([str(i),5])
+    array.append([str(n+1),20])
+    print(array)
+    return array
+
 
 @app.route("/")
 def hello():
@@ -30,10 +91,26 @@ def requests_count():
     for k,v in graphs.items():
         res.append(prometheus_client.generate_latest(v))
     return Response(res, mimetype="text/plain")
+#For testing if db actually works.
+@app.route("/show")
+def show_db():
+    for i in range (0,3):
+        add_new_row(random.randint(1,10000));
+    return(get_all())
 
+# API usage and sent output to prometheus.
 @app.route("/generate")
 def generate_data():
-    while True:
-        num = anomaly_series[random.randint(0,9)]
-        time.sleep(1.000)
-        graphs['f'].set(num)
+    anomalyArray = generateArray(20)
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'datalake.json'
+    try:
+        ret = predict_json('coca-cola-datalake-dev', model='anomaly_detector', instances=[], data=anomalyArray)
+    except Exception as e:
+        print(e)
+    for i in range (len(ret)):
+        time.sleep(0.600)
+        graphs['f'].set(ret[i][1])
+    return "Metrics are sent !"
+
+if __name__ == '__main__':
+    app.run(debug=True)
